fc5173164724f1c81d5171d6049a23810cb602b7
AnyCoder <dobrotek6@gmail.com>
2018-01-22 21:26:20 +0100

    Initial commit with ready to use template for Django, VueJS, Social Authentication and RESTFull MongoDB working together with PostreSQL.

---
+++ frontend/src/components/Dismiss.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Sorry, you can't log in!
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss">
+</style>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>
+            Welcome home!
+        </div>
+        <div>
+            <button @click="authenticate('google')">auth Google</button>
+        </div>
+    </div>
+</template>
+<script>
+import { authMixin } from '../mixins/authMixin'
+export default {
+    mixins: [ authMixin ],
+}
+</script>
+<style lang="scss">
+</style>
